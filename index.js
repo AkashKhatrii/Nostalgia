4d26fa79597593b842d5d17b84bb6922616f64bb
AkashKhatrii <65164913+AkashKhatrii@users.noreply.github.com>
2021-07-20 23:03:29 +0530

    Add like feature

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ const app = express();
 const Blog = require('./models/blog.js');
 const Memory = require('./models/memory.js');
 const User = require('./models/user.js')
+const Like = require("./models/Like.js")
+
+
+// IMPORTING ROUTES
+const blogRoutes = require("./routes/blogRoutes.js")
+
 
 // MIDDLEWARES
 app.use(express.json());
@@ -20,6 +26,15 @@ app.use(express.urlencoded({
 }));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
+
+app.use( function( req, res, next ) {
+
+    if ( req.query._method == 'PUT' ) {
+        req.method = 'PUT';
+        req.url = req.path;
+    }       
+    next(); 
+});
 
 
 // CONNECTING TO DATABASE
@@ -30,7 +45,7 @@ mongoose.connect("mongodb://localhost:27017/NostalgiaDB", {useNewUrlParser: true
 
 var loggedIn = false
 var loggedInUser = ""
-
+var msg = ''
 
 var storage = multer.diskStorage({
     destination: "./public/uploads/",
@@ -44,16 +59,86 @@ var upload = multer({
 }).single('memoryImage');
 
 
+// ROUTES
+
+app.post("/register", (req, res) => {
+    const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        confirmpassword: req.body.confirm
+    })
+
+    newUser.save((err) => {
+        if(!err){
+            res.redirect("/register")
+            console.log("Added")
+        }
+    })
+})
+
+app.post("/login", (req, res) => {
+    var userName = req.body.username;
+    var password = req.body.userpassword;
+   
+    User.findOne({ email: userName})
+    .then((result) => {
+        if(password == result.password){
+            res.redirect("/")
+            loggedIn = true
+            loggedInUser = result.email
+            console.log(loggedInUser)
+        console.log(result)
+        }
+        else{
+
+            msg = "Incorrect Password."
+            res.redirect("/login")
+            
+        }
+    })
+    .catch((err) => {
+
+        msg = "Sorry, Couldn't find your account. Please register and log in."
+            res.redirect("/login")
+        
+    })
+})
+
+app.get("/register", (req, res) =>{
+    res.render("register")
+})
+
+app.get("/login", (req, res) => {
+    res.render("login", { message: msg })
+})
+
+app.get("/loginProceed", (req, res) => {
+    if(!loggedIn){
+        
+    res.render("loginProceed")
+    }
+})
+
+
 
 app.get("/", function(req, res){
 
-        res.render("home");
-      
-    
+    res.render("home");
+  
+
 })
+
+
 
 app.get("/memories", function(req, res){
 
+    if(!loggedIn){
+        
+        res.redirect("/loginProceed")
+      
+    }
+    else{
     Memory.find().sort({ createdAt: -1 })
     .then((result) => {
         res.render("memoriesP", {
@@ -65,65 +150,20 @@ app.get("/memories", function(req, res){
     .catch((err) => {
         console.log(err);
     })   
-    // Memory.find({}, function(err, foundMemories){
-    //     res.render("memoriesP", {
-    //       displayMemories: foundMemories
-    //       });
-    //   });
+}
     
 });
 
 
-app.get("/userBlog", function(req,res){
-    res.render("blogForm")
-})
-
-app.get("/blogs", function(req, res){
-
+app.get("/userMemory", function(req, res){
     if(!loggedIn){
         
         res.redirect("/loginProceed")
       
     }
     else{
-    Blog.find().sort({ createdAt: -1})
-    .then((result) => {
-        res.render("blogs", {
-            displayBlogs: result
-            });
-        })
-    .catch((err) => {
-        console.log(err);
-    });
-}
-
-
-    // })
-    // Blog.find({}, function(err, foundBlogs){
-    //     res.render("blogs", {
-    //       displayBlogs: foundBlogs
-    //       });
-    //   });
-    
-});
-
-
-app.get("/blogs/:blogId", function(req, res){
-    const requestedBlogId = req.params.blogId;
-    Blog.findOne({_id: requestedBlogId}, function(err, blog){
-        res.render("blog", {
-            title: blog.title,
-            intro: blog.intro,
-            body1: blog.body1,
-            body2: blog.body2,
-            body3: blog.body3,
-            conclusion: blog.conclusion,
-        });
-      });
-})
-
-app.get("/userMemory", function(req, res){
     res.render("memoryForm");
+    }
 });
 
 
@@ -147,7 +187,54 @@ app.post("/userMemory", upload, function(req, res){
 })
 
 
-app.post("/userBlog", upload, function(req, res){
+
+
+app.get("/userBlog", function(req,res){
+    if(!loggedIn){
+        
+        res.redirect("/loginProceed")
+      
+    }
+    else{
+    res.render("blogForm")
+    }
+});
+
+app.get("/blogs", function(req, res){
+
+    if(!loggedIn){
+        
+        res.redirect("/loginProceed")
+      
+    }
+    else{
+    Blog.find().sort({ createdAt: -1})
+    .then((result) => {
+        res.render("blogs", {
+            displayBlogs: result
+            });
+        })
+    .catch((err) => {
+        console.log(err);
+    });
+}});
+
+app.get("/blogs/:blogId", function(req, res){
+    const requestedBlogId = req.params.blogId;
+    Blog.findOne({_id: requestedBlogId}, function(err, blog){
+        res.render("blog", {
+            title: blog.title,
+            intro: blog.intro,
+            body1: blog.body1,
+            body2: blog.body2,
+            body3: blog.body3,
+            conclusion: blog.conclusion,
+        });
+      });
+});
+
+
+app.post("/userBlog", function(req, res){
     const newBlog = new Blog({
         title: req.body.blogTitle,
         intro: req.body.blogIntro,
@@ -166,58 +253,37 @@ app.post("/userBlog", upload, function(req, res){
    });
  
  
- })
+ });
 
 
-app.post("/register", (req, res) => {
-    const newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        confirmpassword: req.body.confirm
-    })
 
-    newUser.save((err) => {
-        if(!err){
-            res.redirect("/register")
-            console.log("Added")
+app.put( '/memories/:memoryId', function ( req, res ) {
+    // delete operation stuff
+    const likememory = req.params.memoryId
+    console.log(typeof(likememory))
+    console.log(likememory)
+    console.log(loggedInUser)
+    
+    User.findOneAndUpdate({email: loggedInUser},
+        {$addToSet: {memorylikes: likememory}},
+        {safe: true, upsert: true},
+        function(err, doc) {
+            if(err){
+            console.log(err);
+            }else{
+            console.log("Updated")
+            }
         }
-    })
-})
+    );
+    
 
-app.post("/login", (req, res) => {
-    var userName = req.body.username;
-    var password = req.body.userpassword;
+    
+    res.redirect("/memories")
+  });
 
-    User.findOne({ email: userName})
-    .then((result) => {
-        if(password == result.password){
-            res.redirect("/")
-            loggedIn = true
-            loggedInUser = result.email
-            console.log(loggedInUser)
-        console.log(result)
-        }
-        else{
-            res.redirect("/login")
-            console.log("Bhaag")
-        }
-    })
-    .catch((err) => {
-        console.log("No user");
-    })
-})
 
-app.get("/register", (req, res) =>{
-    res.render("register")
-})
-
-app.get("/login", (req, res) => {
-    res.render("login")
-})
-
-app.get("/loginProceed", (req, res) => {
-    res.render("loginProceed")
+app.get("/user/:userId/savedPosts", (req, res) => {
+    res.send("Hi");
 })
 
 app.listen(3000, function(){
